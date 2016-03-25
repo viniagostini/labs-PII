af40dbd6377df8b9c18d563d4b49c60bba44deb0
viniagostini <viniciusaagostini@gmail.com>
2016-03-24 23:30:28 -0300

    Testes de jogos criados

---
+++ Lab-06/p2-cgTests/jogo/JogoLutaTest.java
@@ -0,0 +1,29 @@
+package jogo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JogoLutaTest {
+
+	@Test
+	public void testRegistraJogada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJogoLuta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdicionaJogabilidade() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
