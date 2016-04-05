86d3ca6d92b154ab8ad67c8b90b73e4a8d1517c2
viniciusaa <viniciusaa@lcc1-29.lcc.ufcg.edu.br>
2016-04-05 09:35:11 -0300

    Factory de Usuario criada

---
+++ Lab-06/p2-cg/factory/UsuarioFactory.java
@@ -0,0 +1,21 @@
+package factory;
+
+import jogo.JogoLuta;
+import jogo.JogoPlataforma;
+import jogo.JogoRPG;
+import exceptions.LogicaDeNegociosExecption;
+import usuario.Usuario;
+
+public class UsuarioFactory {
+
+	
+	public Usuario criaUsuario(String nome, String login){
+		
+		//implementar
+		
+		
+		return null;
+	}
+	
+	
+}
