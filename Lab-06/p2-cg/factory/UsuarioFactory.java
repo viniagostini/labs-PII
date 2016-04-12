75b360958eff17e5439dad5cf11c8c9a31ae899c
viniagostini <viniciusaagostini@gmail.com>
2016-04-12 01:05:11 -0300

    UsuarioFactory implementado

--- Lab-06/p2-cg/factory/UsuarioFactory.java
+++ Lab-06/p2-cg/factory/UsuarioFactory.java
@@ -1,20 +1,39 @@
 package factory;
 
+import jogo.Jogo;
 import jogo.JogoLuta;
 import jogo.JogoPlataforma;
 import jogo.JogoRPG;
+import exceptions.DadosInvalidosException;
 import exceptions.LogicaDeNegociosExecption;
+import usuario.TipoDeUsuario;
 import usuario.Usuario;
+import usuario.UsuarioNoob;
+import usuario.UsuarioVeterano;
 
 public class UsuarioFactory {
 
 	
-	public Usuario criaUsuario(String nome, String login){
+	public Usuario criaUsuario(String nomeUsuario, String loginUsuario, TipoDeUsuario tipoUsuario)throws DadosInvalidosException, LogicaDeNegociosExecption{
 		
-		//implementar
+		Usuario novoUsuario;
 		
+		switch (tipoUsuario) {
+		case NOOB:
+			
+			novoUsuario = new UsuarioNoob(nomeUsuario, loginUsuario);
+			
+			break;
+		case VETERANO:
+			
+			novoUsuario = new UsuarioVeterano(nomeUsuario, loginUsuario);
+			
+			break;
+		default:
+			throw new LogicaDeNegociosExecption("Tipo de usuario invalido");
+		}
 		
-		return null;
+		return novoUsuario;
 	}
 	
 	
